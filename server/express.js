7cfa66881762ae25f579e8297a48852753419d5b
Codemaniac6 <56360107+Codemaniac6@users.noreply.github.com>
2022-03-01 17:10:35 +0100

    added authentication routes and controllers

--- server/express.js
+++ server/express.js
@@ -7,6 +7,8 @@ import helmet from 'helmet';
 import config from './../config/config'
 import Template from './../template'
 import userRoutes from './routes/user.routes'
+import authRoutes from './routes/auth.routes'
+
 // configure express
 const app = express();
 
@@ -18,13 +20,22 @@ app.use(helmet());
 app.use(cors());
 
 // Routes
-app.use('/', userRoutes)
+app.use('/', userRoutes);
+app.use('/', authRoutes);
 
 
 app.get('/', async (req, res) => {
     res.status(200).send(Template())
-})
+});
 
+app.use((err, req, res, next) => {
+    if (err.name === "UnauthorizedError") {
+        res.status(401).json({ "error": err.name + ":" + err.message })
+    } else if (err) {
+        res.status(400).json({ "error": err.name + ":" + err.message })
+        console.log(err)
+    }
+});
 
 
 export default app
